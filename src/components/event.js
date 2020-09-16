bd7b6c0dbcb3fc8a35c458d280e8c7db21f70702
sdserranoa <47216137+sdserranoa@users.noreply.github.com>
2020-09-16 00:26:16 -0500

    Update event.js

--- src/components/event.js
+++ src/components/event.js
@@ -14,7 +14,7 @@ export default class Event extends React.Component {
     }
 
     componentDidMount() {
-        fetch('http://35.198.15.156:8080/events/' + this.props.match.params.id, {
+        fetch('http://3.131.142.53:8080/events/' + this.props.match.params.id, {
             method: 'GET',
             headers: {
                 'authorization': localStorage.getItem("token"),
